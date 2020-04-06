0be8acbf708172a4b929f77b552f080dcd3dc2cc
Radu Sorostinean <radu.sorostinean@gmail.com>
2020-04-06 18:00:16 +0100

    fixes

--- meal-prep/src/components/AppNavigation.vue
+++ meal-prep/src/components/AppNavigation.vue
@@ -3,17 +3,17 @@
         <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
             <v-list>
                 <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content>
+                    <v-list-item :key="index">
+                        <v-list-item-content>
                             {{item.title}}
-                        </v-list-tile-content>
-                    </v-list-tile>
+                        </v-list-item-content>
+                    </v-list-item>
                     <v-divider :key="`divider-${index}`"></v-divider>
                 </template>
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
             <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
